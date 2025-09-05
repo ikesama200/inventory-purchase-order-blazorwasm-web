84f2d4e1e83f65e784fafde54d90a4abd1ec97a9
ikesama200 <y_ikemoto@shikisai.jp.net>
2025-09-06 04:33:47 +0900

    在庫一覧のWebAPIからデータを取得できるように修正

---
+++ Converters/DateTimeSpaceJsonConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace InventoryPurchaseOrderWeb.Converters
+{
+    public class DateTimeSpaceJsonConverter : JsonConverter<DateTime>
+    {
+        private readonly string[] _formats = { "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd" };
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            string? str = reader.GetString();
+            if (str != null && DateTime.TryParseExact(str, _formats, CultureInfo.InvariantCulture,
+                                                      DateTimeStyles.None, out var dt))
+            {
+                return dt;
+            }
+            throw new JsonException($"Cannot convert {str} to DateTime");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
+}
---
+++ Utils/JsonOptions.cs
@@ -0,0 +1,17 @@
+﻿// Helpers/JsonOptions.cs
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using InventoryPurchaseOrderWeb.Converters;  // ここで名前空間を追加
+
+namespace InventoryPurchaseOrderWeb.Utils
+{
+    public static class JsonOptions
+    {
+        public static readonly JsonSerializerOptions Default = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            Converters = { new DateTimeSpaceJsonConverter() } // 先ほど作ったカスタムコンバータ
+        };
+    }
+}
